a49a8eb85ba707774d269b81e36180e76d570793
Isaiah <isaiah03@stanford.edu>
2022-12-04 00:35:21 +0000

    Update 5 files

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar/NavBar";
 import styles from "../styles/Home.module.css";
 import Logo from "../resources/Sponge-Text-Logo.png";
 import HeroPhoto from "../resources/Home-Photo-1.png";
+import Mailchimp from "react-mailchimp-subscribe";
 
 export default function Home() {
   return (
@@ -22,7 +23,22 @@ export default function Home() {
           position={"fixed"}
         ></Image>
         <h1 className={styles.title}>Revolutionizing Online Math Help.</h1>
-        <p className={styles.description}>Check back soon for more updates!</p>
+        <p className={styles.buttonHeader}>
+          Help us design our platform for YOUR needs by taking 30 seconds to
+          answer a few easy questions.
+        </p>
+        <button
+          href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAANAAc13tdlUN0M3T0tJQ0M2QzNYVE5TTEtITldKV0dHTi4u"
+          className={styles.emailButton}
+        >
+          Share my input.
+        </button>
+        <p className={styles.buttonHeader}>
+          Want to be notified when we release our beta?
+        </p>
+        <button href="http://eepurl.com/ifk9gX" className={styles.emailButton}>
+          Sign me up!
+        </button>
       </main>
 
       <footer className={styles.footer}>
